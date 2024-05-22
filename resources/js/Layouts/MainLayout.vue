83b0b7cc7774963f87ccb1c9e1c21a04149fe303
sdnn44 <slidennnq@gmail.com>
2024-05-22 16:56:01 +0200

    Initial commit

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+import { Link } from '@inertiajs/vue3';
+
+import Magnify from 'vue-material-design-icons/Magnify.vue';
+
+let showCreatePost = ref(false);
+</script>
+
+<template>
+    <div id="MainLayout" class="bg-red-600">
+        <div class="div">MainLayout</div>
+        <slot />
+    </div>
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <Head title="Welcome" />
+    <p class="text-blue-100">hi!</p>
+    <MainLayout>
+        <div class="div-color">Home.VUE</div>
+        <p class="text-red-400 mx-24">Helloo!</p>
+    </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from '@/Layouts/MainLayout.vue';
+</script>
+
+<style >
+    .div-color {
+        background: green;
+    }
+</style>
