3887529635061f9d26e0468f9e9955c4ab489640
sdnn44 <slidennnq@gmail.com>
2024-06-13 13:45:10 +0200

    Added relations and controller endpoints

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,63 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\AllPostsCollection;
+use App\Models\Post;
+use App\Models\User;
+
+use App\Services\FileService;
+
 use Illuminate\Http\Request;
+
+use Inertia\Inertia;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+
+    public function show($id)
     {
-        //
+        $user = User::find($id);
+        if ($user === null) {
+            return redirect()->route('home.index');
+        }
+
+        $posts = Post::where('user_id', $id)->orderBy('created_at', 'desc')->get();
+
+        return Inertia::render('User', [
+            'user' => $user,
+            'postByUser' => new AllPostsCollection($posts)
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function update(Request $request)
     {
-        //
-    }
+        $request->validate(['file' => 'required|mimes:jpg,jpeg,png']);
+        $user = (new FileService)->updateFile(auth()->user(), $request, 'user');
+        $user->save();
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return redirect()->route('users.show', ['id' => auth()->user()->id]);
     }
 }
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class); // every comment belongs to post
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class); // every comment belongs to user
+    }
 }
